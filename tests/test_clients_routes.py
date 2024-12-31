1732f4cfc42057802170cc824cc3892a0ba3b02a
marcus-bilt <marcus.santos@biltrewards.com>
2024-12-31 17:14:02 -0300

    Fix unit tests coverage

--- tests/test_clients_routes.py
+++ tests/test_clients_routes.py
@@ -1,16 +1,20 @@
 import unittest
+
+from flask_restful import Api
+
 from app import app
 from app.models.database import db
-from prometheus_client import CONTENT_TYPE_LATEST
 
 class FlaskRoutesTestCase(unittest.TestCase):
 
     def setUp(self):
         """Set up test client before each test."""
         self.app = app
+        self.api = Api(self.app)
         self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # In-memory database for testing
         self.app.config['TESTING'] = True
         self.client = app.test_client()
+        self.client.testing = True
         # Create the database tables
         with self.app.app_context():
             db.create_all()
@@ -22,24 +26,6 @@ class FlaskRoutesTestCase(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    def test_api_main_route(self):
-        """Test the API main route."""
-        response = self.client.get('/')
-        print(response)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Hello World", response.get_json()["message"])
-
-    def test_api_metrics_route(self):
-        """Test the API metrics route."""
-        response = self.client.get('/metrics')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, CONTENT_TYPE_LATEST)
-
-    def test_nonexistent_route(self):
-        """Test a nonexistent route."""
-        response = self.client.get('/nonexistent')
-        self.assertEqual(response.status_code, 404)
-
     def test_api_list_clients_route(self):
         """Test the API list clients route."""
         response = self.client.get('/clients')
@@ -48,22 +34,37 @@ class FlaskRoutesTestCase(unittest.TestCase):
 
     def test_api_add_client_route(self):
         """Test the API add client route."""
-        response = self.client.post('/client/marcus')
+        response = self.client.post('/client', json={"name": "Marcus"})
         self.assertEqual(response.status_code, 200)
+
+    def test_api_add_client_route_error(self):
+        """Test the API add client route with wrong input data."""
+        response = self.client.post('/client', json={})
+        self.assertEqual(response.status_code, 400)
 
     def test_api_get_client_route(self):
         """Test the API get client route."""
-        response = self.client.post('/client/marcus')
+        response = self.client.post('/client', json={"name": "Marcus"})
         self.assertEqual(response.status_code, 200)
-        response = self.client.get('/client/marcus')
+        response = self.client.get('/client/1')
         self.assertEqual(response.status_code, 200)
+
+    def test_api_get_client_route_error(self):
+        """Test the API get client route that does not exist in the database."""
+        response = self.client.get('/client/1')
+        self.assertEqual(response.status_code, 404)
 
     def test_api_delete_client_route(self):
         """Test the API delete client route."""
-        response = self.client.post('/client/marcus')
+        response = self.client.post('/client', json={"name": "Marcus"})
         self.assertEqual(response.status_code, 200)
-        response = self.client.delete('/client/marcus')
+        response = self.client.delete('/client/1')
         self.assertEqual(response.status_code, 200)
+
+    def test_api_delete_client_route_error(self):
+        """Test the API delete client route that does not exist in the database."""
+        response = self.client.delete('/client/1')
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
